895f0662c753527de883b42b7d49e887b85866c5
Jan Pazdziora <jpazdziora@redhat.com>
2018-05-03 18:48:58 +0000

    make clean && make generated_files && hack/update-generated-protobuf-dockerized.sh

--- pkg/apis/core/v1/zz_generated.conversion.go
+++ pkg/apis/core/v1/zz_generated.conversion.go
@@ -3667,6 +3667,7 @@ func autoConvert_core_PodSecurityContext_To_v1_PodSecurityContext(in *core.PodSe
 	// INFO: in.HostNetwork opted out of conversion generation
 	// INFO: in.HostPID opted out of conversion generation
 	// INFO: in.HostIPC opted out of conversion generation
+	// INFO: in.HostUserNamespace opted out of conversion generation
 	// INFO: in.ShareProcessNamespace opted out of conversion generation
 	out.SELinuxOptions = (*v1.SELinuxOptions)(unsafe.Pointer(in.SELinuxOptions))
 	out.RunAsUser = (*int64)(unsafe.Pointer(in.RunAsUser))
@@ -3743,6 +3744,7 @@ func autoConvert_v1_PodSpec_To_core_PodSpec(in *v1.PodSpec, out *core.PodSpec, s
 	// INFO: in.HostNetwork opted out of conversion generation
 	// INFO: in.HostPID opted out of conversion generation
 	// INFO: in.HostIPC opted out of conversion generation
+	// INFO: in.HostUserNamespace opted out of conversion generation
 	// INFO: in.ShareProcessNamespace opted out of conversion generation
 	if in.SecurityContext != nil {
 		in, out := &in.SecurityContext, &out.SecurityContext
--- staging/src/k8s.io/api/core/v1/zz_generated.deepcopy.go
+++ staging/src/k8s.io/api/core/v1/zz_generated.deepcopy.go
@@ -3805,6 +3805,15 @@ func (in *PodSpec) DeepCopyInto(out *PodSpec) {
 			**out = **in
 		}
 	}
+	if in.HostUserNamespace != nil {
+		in, out := &in.HostUserNamespace, &out.HostUserNamespace
+		if *in == nil {
+			*out = nil
+		} else {
+			*out = new(bool)
+			**out = **in
+		}
+	}
 	if in.ShareProcessNamespace != nil {
 		in, out := &in.ShareProcessNamespace, &out.ShareProcessNamespace
 		if *in == nil {
